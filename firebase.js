625578753ed17af657324c3ca873527d327c31b1
prantoraj1856 <prantoraj1856@gmail.com>
2026-02-13 23:22:53 +0600

    Refactor Firebase auth export and config comments

--- firebase.js
+++ firebase.js
@@ -2,19 +2,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-// Your Firebase Config
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBZgNI29JXE4jywdgC_Pnyw_FV2vw2KYAw",
   authDomain: "pranto-ff.firebaseapp.com",
   projectId: "pranto-ff",
   storageBucket: "pranto-ff.firebasestorage.app",
   messagingSenderId: "936271925064",
-  appId: "1:936271925064:web:f1710b460c94ff1aee2d7d"
+  appId: "1:936271925064:web:f1710b460c94ff1aee2d7d",
+  measurementId: "G-V3SWSEZWC3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-// Export auth
-export { auth };
+// Export Auth
+export const auth = getAuth(app);
