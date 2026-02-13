a34b34c689210cde635a4c62b33e6da815b33bab
prantoraj1856 <prantoraj1856@gmail.com>
2026-02-13 21:56:21 +0600

    Set up Firebase app and authentication
    Initialize Firebase app with configuration and authentication.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBZgNI29JXE4jywdgC_Pnyw_FV2vw2KYAw",
+  authDomain: "pranto-ff.firebaseapp.com",
+  projectId: "pranto-ff",
+  storageBucket: "pranto-ff.firebasestorage.app",
+  messagingSenderId: "936271925064",
+  appId: "1:936271925064:web:f1710b460c94ff1aee2d7d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
