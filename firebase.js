e8e2fd1e6bff387e48bfb73e60e33c891c94a100
prantoraj1856 <prantoraj1856@gmail.com>
2026-02-13 22:45:38 +0600

    Upgrade Firebase SDK to version 12.9.0

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
+// Your Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBZgNI29JXE4jywdgC_Pnyw_FV2vw2KYAw",
   authDomain: "pranto-ff.firebaseapp.com",
@@ -11,5 +12,9 @@ const firebaseConfig = {
   appId: "1:936271925064:web:f1710b460c94ff1aee2d7d"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+// Export auth
+export { auth };
